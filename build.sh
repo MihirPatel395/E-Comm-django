128291ed69b7e71d298bf604ca7fae53f79688b4
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-03-30 00:48:25 +0530

    Update command build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 echo "BUILD Start"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+# build_files.sh
+pip install -r requirements.txt
+python3.9.6 manage.py collectstatic
 echo "BUILD End"
