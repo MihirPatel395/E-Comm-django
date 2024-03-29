629e7afa59ea87dc3d620744adc84dbdab6d93f8
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-03-30 00:58:38 +0530

    add static and media url  urls.py

--- E_Commerce/urls.py
+++ E_Commerce/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path,include
 
 urlpatterns = [
@@ -6,3 +8,5 @@ urlpatterns = [
     # path('authenticate/', include('authentication.urls')),
     path('', include('products.urls')),
 ]
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
