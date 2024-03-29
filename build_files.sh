17ded7ab2b3b0b94725f2f916bde269ee3482ca0
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-03-30 01:10:04 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,12 @@
 echo "BUILD Start"
 # build_files.sh
-pip install -r requirements.txt
-python3.9.6 manage.py collectstatic
+python3 pip install -r requirements.txt
+
+echo "Migrating Databases"
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting Static Files"
+python3 manage.py collectstatic --noinput
+
 echo "BUILD End"
