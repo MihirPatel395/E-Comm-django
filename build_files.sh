c2558aa62c970846f9b6da4a02608c6d1d11209b
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-03-30 01:27:37 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,9 @@ echo "BUILD Start"
 # build_files.sh
 python3 pip install -r requirements.txt
 
-echo "Migrating Databases"
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# echo "Migrating Databases"
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
 
 echo "Collecting Static Files"
 python3 manage.py collectstatic --noinput
