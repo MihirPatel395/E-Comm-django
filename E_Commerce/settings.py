5d43384119f6bb585213af9c02139eac57d1442e
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-04-05 09:33:52 +0530

    Update database_url settings.py

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get("DEBUG",False)
 
 ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 # ALLOWED_HOSTS = ['.vercel.app']
 
 
