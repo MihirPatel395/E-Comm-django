48aba2e6267e8639cd596933c5e081935d67fcfc
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-04-15 22:29:06 +0530

    add host settings.py

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG",False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://e-comm-django.onrender.com"]
 # ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 # ALLOWED_HOSTS = ['.vercel.app']
 
