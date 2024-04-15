8c4d4095cb2e02a1de27311ca50fe4116195c05d
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-04-15 22:33:46 +0530

    Update settings.py

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -89,9 +89,6 @@ DATABASES = {
     }
 }
 
-
-database_url=os.environ.get("DATABASE_URL")
-# DATABASES['default']=dj_database_url.parse(database_url)
 DATABASES = {
 	"default": dj_database_url.parse(os.environ.get("database_external_url"))
 }
