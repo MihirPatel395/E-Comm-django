318a165cd3bb99250739826f7fe9f95bc42c51c7
Mihir Patel <72548488+MihirPatel395@users.noreply.github.com>
2024-04-15 22:30:20 +0530

    Update settings.py

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG",False)
 
-ALLOWED_HOSTS = ["https://e-comm-django.onrender.com"]
+# ALLOWED_HOSTS = ["https://e-comm-django.onrender.com"]
 # ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
-# ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
